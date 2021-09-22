065cba03c44e88cf40a63242e48e35107c709d89
leandrolobos <leandrogomezlobos@gmail.com>
2021-09-22 05:54:42 -0300

    Modularización del template

--- carrito.php
+++ carrito.php
@@ -1,37 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>shop - carrito</title>
-        <link rel="icon" type="image/x-icon" href="img/favicon.png" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+      <?php include('./head.php') ?>
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="index.html"><img src="img/logo.png" /></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.html">home</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="login.html">ingresar</a></li>
-                        <li class="nav-item"><a class="nav-link active-link js-scroll-trigger" href="carrito.html"><i class="fas fa-shopping-basket"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php include('./navbar.php') ?>
         <!-- Carrito-->
         <section class="carrito-section" id="carrito">
             <div class="container">
@@ -71,8 +45,6 @@
                         </tr>
                       </tbody>
                     </table>
-
-
                   </div>
                 </div>
                 <div class="row justify-content-end">
@@ -80,9 +52,7 @@
                 </div>
             </div>
         </section>
-        <div class="fluid-container img-footer"></div>
-
-
+        <?php include('./footer.php') ?>
 
         <!-- Bootstrap core JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- index.php
+++ index.php
@@ -1,53 +1,16 @@
 <?php
-  $productos = [
-    ['nombre' => 'Papas fritas', 'precio' => 100, 'imagen' => './img/papas-fritas.png'],
-    ['nombre' => 'Ensalada', 'precio' => 300, 'imagen' => './img/ensalada.png'],
-    ['nombre' => 'Pizza', 'precio' => 500, 'imagen' => './img/pizza.png'],
-    ['nombre' => 'Sándwich', 'precio' => 300, 'imagen' => './img/sandwich.png'],
-    ['nombre' => 'Hamburguesa', 'precio' => 350, 'imagen' => './img/hamburguesa.png'],
-    ['nombre' => 'Galletitas', 'precio' => 200, 'imagen' => './img/galletitas.png'],
-    ['nombre' => 'Pancho', 'precio' => 200, 'imagen' => './img/pancho.png'],
-    ['nombre' => 'Dona', 'precio' => 100, 'imagen' => './img/dona.png']
-  ]
- ?>
+include('./funciones.php');
+$productos = traer_productos();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>shop - home</title>
-        <link rel="icon" type="image/x-icon" href="img/favicon.png" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+      <?php include('./head.php') ?>
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="index.html"><img src="img/logo.png" /></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#productos">productos</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contacto">contacto</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="login.html">ingresar</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="carrito.html"><i class="fas fa-shopping-basket"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php include('./navbar.php') ?>
         <!-- Masthead-->
         <header class="masthead">
             <div class="container d-flex h-100 align-items-center">
@@ -60,7 +23,6 @@
               <div class="col-6">
                 <img src="img/hero.png"/>
               </div>
-
             </div>
         </header>
         <!-- Productos-->
@@ -73,7 +35,6 @@
                 </div>
                 <div class="row align-items-end">
                   <?php foreach ($productos as $producto){ ?>
-
                   <div class="col-xl-3 col-6 mt-4">
                     <div class="producto text-center p-4">
                       <img src="<?php echo $producto['imagen'] ?>" />
@@ -82,59 +43,12 @@
                       <button class="btn btn-agregar">Agregar</button>
                     </div>
                   </div>
-
                 <?php } ?>
-
                 </div>
             </div>
         </section>
         <!-- Contacto-->
-        <section class="contacto-section" id="contacto">
-            <div class="container">
-              <div class="row mx-auto text-center">
-                <div class="col-lg-8 mx-auto">
-                    <h3 class="mb-4">contacto</h3>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-lg-8 col-12 mx-auto">
-                <form>
-                  <div class="row mb-3">
-                    <div class="col">
-                      <input type="text" class="form-control" placeholder="Nombre" id="name">
-                    </div>
-                    <div class="col">
-                      <input type="text" class="form-control" placeholder="Apellido" id="lastname">
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <div class="col">
-                      <input type="text" class="form-control" placeholder="Teléfono" id="phone">
-                    </div>
-                    <div class="col">
-                      <input type="text" class="form-control" placeholder="Email" id="email">
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <div class="col">
-                      <textarea type="text" class="form-control" placeholder="Mensaje" id="message"></textarea>
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <div class="col">
-                    </div>
-                    <div class="col">
-                      <button type="submit" class="btn btn-enviar">Enviar</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              </div>
-
-            </div>
-          <footer class="footer small text-center"><div class="container">Plantilla web didáctica - Potrero Digital 2021</div></footer>
-        </section>
-
+        <?php include('./footer.php') ?>
 
         <!-- Bootstrap core JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
